5604f26fbc4fd704a8ad83fca0815f8146c15d9b
Tri Le <tri.le.mail@gmail.com>
2022-09-21 16:25:14 -0500

    Initial Commit

---
+++ DemoProject/DemoProject.h
@@ -0,0 +1,18 @@
+//
+//  DemoProject.h
+//  DemoProject
+//
+//  Created by Tri Le on 9/21/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoProject.
+FOUNDATION_EXPORT double DemoProjectVersionNumber;
+
+//! Project version string for DemoProject.
+FOUNDATION_EXPORT const unsigned char DemoProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoProject/PublicHeader.h>
+
+
