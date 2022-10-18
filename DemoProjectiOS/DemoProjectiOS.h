74495bb27bbf6dbb05d4cd042eb722d32409c3c1
Tri Le <tri.le.mail@gmail.com>
2022-10-18 17:53:40 -0500

    Add `DemoProjectiOS` framework (prod and test) targets for iOS platform-specific components

---
+++ DemoProjectiOS/DemoProjectiOS.h
@@ -0,0 +1,18 @@
+//
+//  DemoProjectiOS.h
+//  DemoProjectiOS
+//
+//  Created by Tri Le on 10/18/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoProjectiOS.
+FOUNDATION_EXPORT double DemoProjectiOSVersionNumber;
+
+//! Project version string for DemoProjectiOS.
+FOUNDATION_EXPORT const unsigned char DemoProjectiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoProjectiOS/PublicHeader.h>
+
+
