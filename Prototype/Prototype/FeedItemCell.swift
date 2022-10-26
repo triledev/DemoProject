47e6e33d5c35f1787361761605b76fdbda4bbf7b
Tri Le <tri.le.mail@gmail.com>
2022-10-26 14:15:47 -0500

    Add shimmering animation while loading image in the prototype app.

--- Prototype/Prototype/FeedItemCell.swift
+++ Prototype/Prototype/FeedItemCell.swift
@@ -10,6 +10,7 @@ import UIKit
 final class FeedItemCell: UITableViewCell {
     @IBOutlet private(set) var locationContainer: UIView!
     @IBOutlet private(set) var locationLabel: UILabel!
+    @IBOutlet private(set) var feedItemContainer: UIView!
     @IBOutlet private(set) var feedItemView: UIImageView!
     @IBOutlet private(set) var descriptionLabel: UILabel!
 
@@ -17,17 +18,61 @@ final class FeedItemCell: UITableViewCell {
         super.awakeFromNib()
 
         feedItemView.alpha = 0
+        feedItemContainer.startShimmering()
     }
 
     override func prepareForReuse() {
         super.prepareForReuse()
 
         feedItemView.alpha = 0
+        feedItemContainer.startShimmering()
     }
 
     func fadeIn(_ image: UIImage?) {
         feedItemView.image = image
 
-        UIView.animate(withDuration: 0.3, delay: 0.3, options: [], animations: { self.feedItemView.alpha = 1 })
+        UIView.animate(
+            withDuration: 0.25,
+            delay: 1.25,
+            options: [],
+            animations: {
+                self.feedItemView.alpha = 1
+            }, completion: { completed in
+                if completed {
+                    self.feedItemContainer.stopShimmering()
+                }
+            })
+    }
+}
+
+private extension UIView {
+    private var shimmerAnimationKey: String {
+        return "shimmer"
+    }
+
+    func startShimmering() {
+        let white = UIColor.white.cgColor
+        let alpha = UIColor.white.withAlphaComponent(0.7).cgColor
+        let width = bounds.width
+        let height = bounds.height
+
+        let gradient = CAGradientLayer()
+        gradient.colors = [alpha, white, alpha]
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.4)
+        gradient.endPoint = CGPoint(x: 1.0, y: 0.6)
+        gradient.locations = [0.4, 0.5, 0.6]
+        gradient.frame = CGRect(x: -width, y: 0, width: width*3, height: height)
+        layer.mask = gradient
+
+        let animation = CABasicAnimation(keyPath: #keyPath(CAGradientLayer.locations))
+        animation.fromValue = [0.0, 0.1, 0.2]
+        animation.toValue = [0.8, 0.9, 1.0]
+        animation.duration = 1
+        animation.repeatCount = .infinity
+        gradient.add(animation, forKey: shimmerAnimationKey)
+    }
+
+    func stopShimmering() {
+        layer.mask = nil
     }
 }
